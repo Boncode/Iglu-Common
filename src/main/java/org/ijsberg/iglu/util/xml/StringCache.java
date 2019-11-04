89bda0ae861b58fec99f4ecc7be4fea27e001ee7
Jeroen Meetsma <jeroen@ijsberg.nl>
2019-11-04 18:46:07 +0100

    worked on reduction of memory consumption

--- src/main/java/org/ijsberg/iglu/util/xml/StringCache.java
+++ src/main/java/org/ijsberg/iglu/util/xml/StringCache.java
@@ -6,26 +6,37 @@ import java.util.TreeMap;
 
 public abstract class StringCache {
     private static int count = 0;
-    private static Map<String, Integer> numberByString = new TreeMap<>();
-    private static Map<Integer, String> stringByNumber = new TreeMap<>();
+    private static Map<String, Integer> numberByString = new HashMap<>();
+    private static Map<Integer, String> stringByNumber = new HashMap<>();
 
     private StringCache(){}
 
     public static int storeString(String string) {
-        Integer number = numberByString.get(string);
-        if(number == null) {
-            number = count++;
-            numberByString.put(string, number);
-            stringByNumber.put(number, string);
+        synchronized (numberByString) {
+            Integer number = numberByString.get(string);
+            if (number == null) {
+                number = ++count;
+                numberByString.put(string, number);
+                stringByNumber.put(number, string);
+            }
+            return number;
         }
-        return number;
     }
 
     public static String getString(int number) {
-        return stringByNumber.get(number);
+        synchronized (numberByString) {
+            return stringByNumber.get(number);
+        }
     }
 
     public static int getSize() {
         return numberByString.size();
+    }
+
+    public static final void clear() {
+        synchronized (numberByString) {
+            numberByString.clear();
+            stringByNumber.clear();
+        }
     }
 }
