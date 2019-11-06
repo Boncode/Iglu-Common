6342c48959ec0ff732ba66317bf0e6df8c3ad994
Jeroen Meetsma <jeroen@ijsberg.nl>
2019-11-06 13:10:46 +0100

    worked on reduction of memory consumption

--- src/main/java/org/ijsberg/iglu/util/xml/StringCache.java
+++ src/main/java/org/ijsberg/iglu/util/xml/StringCache.java
@@ -1,23 +1,57 @@
 package org.ijsberg.iglu.util.xml;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import javafx.collections.transformation.SortedList;
+
+import java.util.*;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
 
 public abstract class StringCache {
-    private static int count = 0;
+    //private static int count = 0;
     private static Map<String, Integer> numberByString = new HashMap<>();
-    private static Map<Integer, String> stringByNumber = new HashMap<>();
+    //private static Map<Integer, String> stringByNumber = new HashMap<>();
+
+    private static List<String> strings = new ArrayList();
+
+    static {
+        strings.add(null);
+    }
+
+/*    private static final List<String> sortedStrings = new ArrayList<String>() {
+                public boolean add(String string) {
+                    int index = Collections.binarySearch(this, string);
+                    if (index < 0) index = ~index;
+                    super.add(index, string);
+                    return true;
+                }
+            };
+*/
 
     private StringCache(){}
 
+/*
+    public static String getCachedString(String string) {
+        int index = Collections.binarySearch(sortedStrings, string);
+        if(index != -1) {
+            return sortedStrings.get(index);
+        } else {
+            sortedStrings.add(string);
+        }
+        return string;
+    }
+*/
     public static int storeString(String string) {
+
         synchronized (numberByString) {
+           // long start = System.currentTimeMillis();
+            //strings.indexOf(string);
+           // System.out.println(System.currentTimeMillis() - start);
             Integer number = numberByString.get(string);
             if (number == null) {
-                number = ++count;
+                number = strings.size();
                 numberByString.put(string, number);
-                stringByNumber.put(number, string);
+                //stringByNumber.put(number, string);
+                strings.add(string);
             }
             return number;
         }
@@ -25,7 +59,7 @@ public abstract class StringCache {
 
     public static String getString(int number) {
         synchronized (numberByString) {
-            return stringByNumber.get(number);
+            return strings.get(number);
         }
     }
 
@@ -36,7 +70,45 @@ public abstract class StringCache {
     public static final void clear() {
         synchronized (numberByString) {
             numberByString.clear();
-            stringByNumber.clear();
+            strings.clear();
+            strings.add(null);
+        }
+    }
+
+
+
+
+    public static void main(String[] args) {
+        try {
+            // Encode a String into bytes
+            String inputString = "blahblahblah@☺koklkadsblablabla";
+            byte[] input = inputString.getBytes("UTF-8");
+
+            System.out.println(input.length);
+
+            // Compress the bytes
+            byte[] output = new byte[100];
+            Deflater compresser = new Deflater();
+            compresser.setInput(input);
+            compresser.finish();
+            int compressedDataLength = compresser.deflate(output);
+            compresser.end();
+
+            System.out.println(compressedDataLength);
+
+            // Decompress the bytes
+            Inflater decompresser = new Inflater();
+            decompresser.setInput(output, 0, compressedDataLength);
+            byte[] result = new byte[100];
+            int resultLength = decompresser.inflate(result);
+            decompresser.end();
+
+            // Decode the bytes into a String
+            String outputString = new String(result, 0, resultLength, "UTF-8");
+        } catch(java.io.UnsupportedEncodingException ex) {
+            // handle
+        } catch (java.util.zip.DataFormatException ex) {
+            // handle
         }
     }
 }
