d65fbaa91a9f02fcdcee58ee216f6244d6a65fbd
Jeroen Meetsma <jeroen@ijsberg.nl>
2020-06-09 15:49:51 +0200

    added Vopak Excel export

---
+++ src/test/java/org/ijsberg/iglu/usermanagement/module/StandardUserManagerTest.java
@@ -0,0 +1,16 @@
+package org.ijsberg.iglu.usermanagement.module;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StandardUserManagerTest {
+
+    @Test
+    public void getHash() {
+        //dlG5%8sQ
+        //51964d7aab1ae29f3db3322a9137a8cf
+
+        System.out.println(StandardUserManager.passwordsMatch("dlG5%8sQ", "51964d7aab1ae29f3db3322a9137a8cf"));
+    }
+}
