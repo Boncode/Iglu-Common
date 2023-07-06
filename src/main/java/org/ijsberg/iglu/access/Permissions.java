d476d3df5fe232ffb63aa271daeee57cf22231aa
Koen Naarding <koen_naarding97@hotmail.com>
2023-07-06 12:02:51 +0200

    removed RWX rights from API and roles/permissions

--- src/main/java/org/ijsberg/iglu/access/Permissions.java
+++ src/main/java/org/ijsberg/iglu/access/Permissions.java
@@ -9,9 +9,10 @@ public class Permissions {
 
     public static final String FULL_CONTROL = "FULL_CONTROL";
 
-    public static final String R = "r";
-    public static final String W = "w";
-    public static final String X = "x";
+//    public static final String R = "r";
+//    public static final String W = "w";
+//    public static final String X = "x";
+    public static final String UPLOAD = "UPLOAD";
 
     public static final String TENANT_SURPASSING = "TENANT_SURPASSING";
 
@@ -26,10 +27,11 @@ public class Permissions {
 
     static {
         register(FULL_CONTROL, "full control", "Administrator rights");
-        register(R, "read", "Unix-style read access, typically for viewing content only");
-        register(W, "write", "Unix-style write access, typically for creating and modifying content");
-        register(X, "execute", "Unix-style execute access, typically for managing configurations");
+//        register(R, "read", "Unix-style read access, typically for viewing content only");
+//        register(W, "write", "Unix-style write access, typically for creating and modifying content");
+//        register(X, "execute", "Unix-style execute access, typically for managing configurations");
         register(TENANT_SURPASSING, "tenant and user group surpassing", "No restrictions regarding multitenancy, access to resources and functions of all user groups");
+        register(UPLOAD, "generic file uploads", "Generic file uploads");
     }
 
     public static BasicPermission get(String id) {
