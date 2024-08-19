5c8c47fcb55c1f0b7a27b84d7f2e969bcd9742ab
Koen Naarding <koen_naarding97@hotmail.com>
2024-08-19 12:08:30 +0200

    removed static init() from classes, not needed.

--- src/main/java/org/ijsberg/iglu/access/Permissions.java
+++ src/main/java/org/ijsberg/iglu/access/Permissions.java
@@ -7,14 +7,8 @@ import java.util.Map;
 public class Permissions {
 
     public static final String FULL_CONTROL = "FULL_CONTROL";
-
-//    public static final String R = "r";
-//    public static final String W = "w";
-//    public static final String X = "x";
     public static final String UPLOAD = "UPLOAD";
-
     public static final String TENANT_SURPASSING = "TENANT_SURPASSING";
-
 
     protected static final Map<String, BasicPermission> permissionMap = new LinkedHashMap<>();
 
@@ -26,9 +20,6 @@ public class Permissions {
 
     static {
         register(FULL_CONTROL, "full control", "Administrator rights");
-//        register(R, "read", "Unix-style read access, typically for viewing content only");
-//        register(W, "write", "Unix-style write access, typically for creating and modifying content");
-//        register(X, "execute", "Unix-style execute access, typically for managing configurations");
         register(TENANT_SURPASSING, "tenant and user group surpassing", "No restrictions regarding multitenancy, access to resources and functions of all user groups");
         register(UPLOAD, "generic file uploads", "Generic file uploads");
     }
