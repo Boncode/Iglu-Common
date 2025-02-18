8fdde8d40a833a568abc06560f7931f2afd61cc1
Koen Naarding <koen_naarding97@hotmail.com>
2025-02-18 09:17:16 +0100

    cleanup of comments

--- src/main/java/org/ijsberg/iglu/access/BasicUser.java
+++ src/main/java/org/ijsberg/iglu/access/BasicUser.java
@@ -1,21 +1,3 @@
-/*
- * Copyright 2011-2014 Jeroen Meetsma - IJsberg Automatisering BV
- *
- * This file is part of Iglu.
- *
- * Iglu is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Iglu is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Iglu.  If not, see <http://www.gnu.org/licenses/>.
- */
 package org.ijsberg.iglu.access;
 
 import org.ijsberg.iglu.event.messaging.UserConsumableMessage;
@@ -46,15 +28,6 @@ public class BasicUser implements User {
 	private final List<UserConsumableMessage> messageQueue = new ArrayList<>();
 
 
-	//public static BasicUser createAdmin() {
-
-	//}
-
-	/**
-	 * @param userId
-	 * @param roles
-	 * @param settings
-	 */
 	public BasicUser(String userId, List<Role> roles, List<UserGroup> groups, Properties settings) {
 		this.userId = userId;
 		this.settings = settings;
@@ -73,32 +46,18 @@ public class BasicUser implements User {
 		this.settings = settings;
 	}
 
-	/**
-	 * @param userId
-	 */
 	public BasicUser(String userId) {
 		this.userId = userId;
 	}
 
-	/**
-	 * @return
-	 */
 	public final String getId() {
 		return userId;
 	}
 
-
-	/**
-	 * @return the user id
-	 */
 	public String toString() {
 		return userId;
 	}
 
-
-	/**
-	 * @return
-	 */
 	public Properties getSettings() {
 		return settings;
 	}
@@ -129,7 +88,6 @@ public class BasicUser implements User {
 		return false;
 	}
 
-
 	@Override
 	public boolean hasAllRights(String ... rightsIds) {
 		for (String accessRightId : rightsIds) {
@@ -140,19 +98,10 @@ public class BasicUser implements User {
 		return true;
 	}
 
-	/**
-	 * @param roleId
-	 * @return
-	 * @see User#getRoles()
-	 */
 	public Role getRole(String roleId) {
 		return roles.get(roleId);
 	}
 
-	/**
-	 * @return
-	 * @see User#getRoles()
-	 */
 	public Collection<Role> getRoles() {
 		return roles.values();
 	}
@@ -162,9 +111,6 @@ public class BasicUser implements User {
 		this.roles.put(role.getName().trim(), role);
     }
 
-    /**
-	 * @return false
-	 */
 	public boolean isAccountBlocked() {
 		return false;
 	}
